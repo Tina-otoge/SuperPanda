1e07d1d82f63179dd398999c1fc75ca9536332cc
Tina <me@tina.moe>
2020-12-21 05:53:43 +0100

    Remove unused hardcoded EH urls

--- app/sadpanda/pages.py
+++ app/sadpanda/pages.py
@@ -1,5 +1,3 @@
 GALLERIES_SEARCH_URL = 'https://e-hentai.org/?page={page}&f_search={search}&f_cats={category_filter}'
-# GALLERIES_SEARCH_URL = 'https://e-hentai.org/?f_cats=767&f_search={search}' # non-h
-# GALLERIES_URL = 'https://e-hentai.org/?f_cats=1017&f_search={search}' # manga + doujin
 GALLERY_URL = 'https://e-hentai.org/g/{id}/{token}/?p={page}'
 GALLERY_PAGE_URL = 'https://e-hentai.org/s/{token}/{gallery}-{page}'
