f60ed82da56a8fafec9f0f35577d36a1a71fce1d
Tina <skielred@gmail.com>
2020-12-21 23:03:47 +0100

    Remove duplicate "refresh" filter

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,6 @@ from app.utils import LenientJSONEncoder
 
 main = Blueprint('main', __name__)
 
-@main.app_template_filter('refresh')
-def refresh_filter(x):
-    if not current_app.debug:
-        return x
-    return '{}?{}'.format(x, str(random.random())[2:8])
-
 def create_app():
     app = Flask(__name__)
     app.json_encoder = LenientJSONEncoder
