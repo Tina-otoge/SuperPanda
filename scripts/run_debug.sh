c82a6f35c011195c8bc5c940edc2a4d264d19a77
Tina <me@tina.moe>
2021-10-25 07:17:07 +0200

    Add possibility to pass port to helper run script

--- scripts/run_debug.sh
+++ scripts/run_debug.sh
@@ -2,4 +2,4 @@
 
 python -m venv .venv
 ./.venv/bin/pip install -r requirements.txt
-FLASK_RUN_HOST=0.0.0.0 FLASK_DEBUG=1 ./.venv/bin/flask run
+FLASK_RUN_HOST=0.0.0.0 FLASK_DEBUG=1 ./.venv/bin/flask run -p ${2:-8888}
