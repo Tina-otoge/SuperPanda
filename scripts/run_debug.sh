286cc5d4057a3682d7aa69fab4805e3e4e4b0ebf
Tina <me@tina.moe>
2022-10-25 19:12:57 +0200

    Fix run script using wrong argument number for port

--- scripts/run_debug.sh
+++ scripts/run_debug.sh
@@ -2,4 +2,4 @@
 
 python -m venv .venv
 ./.venv/bin/pip install -r requirements.txt
-FLASK_RUN_HOST=0.0.0.0 FLASK_DEBUG=1 ./.venv/bin/flask run -p ${2:-8888}
+FLASK_RUN_HOST=0.0.0.0 FLASK_DEBUG=1 ./.venv/bin/flask run -p ${1:-8888}
